820a16b573b41c42345dc64105dcb2d9e2c176d4
Ryan Mueller <rrmueller@fscinc.com>
2024-03-20 13:23:48 -0500

    Functioning 3D orientation display!
    Well.... there's a few minor problems with it but it basically works.

--- main.cpp
+++ main.cpp
@@ -7,15 +7,65 @@
 // - Introduction, links and more at the top of imgui.cpp
 
 #include "imgui.h"
+#include "imgui_internal.h"
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx11.h"
 #include <d3d11.h>
 #include <tchar.h>
 
-#include "TimersAndCalculations.h"
+// START - 3D PROJECTION
+#include "ImGuizmo.h" // 3D projection
+bool useWindow = true;
+int gizmoCount = 1;
+static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
+
+
+float camDistance = 8.f;
+
+float objectMatrix[4][16] = {
+  { 1.f, 0.f, 0.f, 0.f,
+    0.f, 1.f, 0.f, 0.f,
+    0.f, 0.f, 1.f, 0.f,
+    0.f, 0.f, 0.f, 1.f },
+
+  { 1.f, 0.f, 0.f, 0.f,
+  0.f, 1.f, 0.f, 0.f,
+  0.f, 0.f, 1.f, 0.f,
+  2.f, 0.f, 0.f, 1.f },
+
+  { 1.f, 0.f, 0.f, 0.f,
+  0.f, 1.f, 0.f, 0.f,
+  0.f, 0.f, 1.f, 0.f,
+  2.f, 0.f, 2.f, 1.f },
+
+  { 1.f, 0.f, 0.f, 0.f,
+  0.f, 1.f, 0.f, 0.f,
+  0.f, 0.f, 1.f, 0.f,
+  0.f, 0.f, 2.f, 1.f }
+};
+
+static const float identityMatrix[16] =
+{ 1.f, 0.f, 0.f, 0.f,
+    0.f, 1.f, 0.f, 0.f,
+    0.f, 0.f, 1.f, 0.f,
+    0.f, 0.f, 0.f, 1.f };
+
+void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bool editTransformDecomposition);
+void Perspective(float fovyInDegrees, float aspectRatio, float znear, float zfar, float* m16);
+void Frustum(float left, float right, float bottom, float top, float znear, float zfar, float* m16);
+void LookAt(const float* eye, const float* at, const float* up, float* m16);
+void Normalize(const float* a, float* r);
+float Dot(const float* a, const float* b);
+void Cross(const float* a, const float* b, float* r);
+// END - 3D PROJECTION
+
+#include "TimersAndCalculations.h" // fsc library
 
 //START - PCAN - USB READING
 #include "03_ManualRead.h"
+
+#include <math.h>
+#define PI 3.14159265
 
 ManualRead CAN;
 
@@ -70,6 +120,49 @@ int ExtractBoolFromCanTelegram(uint8_t telegram[], uint8_t sizeOfTelegram, bool*
 int ExtractUint16FromCanTelegram(uint8_t telegram[], uint8_t sizeOfTelegram, uint16_t* output, SPN_Config spnConfig);
 int ExtractUint8FromCanTelegram(uint8_t telegram[], uint8_t sizeOfTelegram, uint8_t* output, SPN_Config spnConfig);
 
+float CalculateYawEuler(float x, float y, float z)
+{
+    float miu = 0.001;
+    int sign = 1;
+    if (z < 0)
+        sign = -1;
+    float yaw = (180.0 / PI) * atan2(-y, sign * sqrt(z * z + miu * x * x));
+    //if (z < 0)
+    //{
+    //    yaw = -yaw;
+    //    if (yaw > 0)
+    //        yaw = 180 - yaw;
+    //    else
+    //        yaw = -180 - yaw;
+    //}
+    return yaw;
+}
+
+float CalculateRollEuler(float x, float y, float z)
+{
+    float miu = 0.001;
+    int sign = 1;
+    if (x < 0)
+        sign = -1;
+    return (180.0 / PI) * atan2(-y, sign * sqrt(x * x + miu * z * z));
+}
+
+float CalculatePitchEuler(float x, float y, float z)
+{
+    //float miu = 0.001;
+    //int sign = 1;
+    //if (x < 0)
+    //    sign = -1;
+    float pitch =  -(180.0 / PI) * atan2(x, sqrt(y * y + z * z));
+    //if (z < 0)
+    //{
+    //    if (pitch > 0)
+    //        pitch = 180 - pitch;
+    //    else
+    //        pitch = -180 - pitch;
+    //}
+    return pitch;
+}
 
 float MM7_C_YAW_RATE;
 float MM7_C_ROLL_RATE;
@@ -262,6 +355,71 @@ int main(int, char**)
                 ImGui::Begin("PCAN USB");                          // Create a window called "Hello, world!" and append into it.TPCANMsg CANMsg;
                 ImGui::Text("YAW RATE: %f\tPITCH RATE: %f\tROLL RATE: %f\t\n", MM7_C_YAW_RATE, MM7_C_PITCH_RATE, MM7_C_ROLL_RATE);
                 ImGui::Text("AY: %f\tAX: %f\tAZ: %f\t\n", MM7_C_AY, MM7_C_AX, MM7_C_AZ);
+
+
+                // START - 3D DISPLAY
+
+                int matId = 0;
+                ImGuizmo::SetID(matId);
+                // start - disregard this stuff?
+                float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+                ImGuizmo::DecomposeMatrixToComponents(objectMatrix[matId], matrixTranslation, matrixRotation, matrixScale);
+                ImGui::InputFloat3("Tr", matrixTranslation);
+                ImGui::InputFloat3("Rt", matrixRotation);
+                ImGui::InputFloat3("Sc", matrixScale);
+                ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, objectMatrix[matId]);
+                // end - disregard this stuff?
+
+
+                float yaw = CalculateYawEuler(MM7_C_AX, MM7_C_AY, MM7_C_AZ);
+                float roll = CalculateRollEuler(MM7_C_AX, MM7_C_AY, MM7_C_AZ);
+                float pitch = CalculatePitchEuler(MM7_C_AX, MM7_C_AY, MM7_C_AZ);
+
+                ImGui::Text("yaw: %f", yaw);
+                ImGui::Text("roll: %f", roll);
+                ImGui::Text("pitch: %f", pitch);
+                matrixRotation[0] = yaw;
+                //matrixRotation[1] = roll;
+                matrixRotation[2] = pitch;
+
+                ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, objectMatrix[matId]);
+                static float cameraView[16] =
+                { 1.f, 0.f, 0.f, 0.f,
+                  0.f, 1.f, 0.f, 0.f,
+                  0.f, 0.f, 1.f, 0.f,
+                  0.f, 0.f, 0.f, 1.f };
+
+                static int lastUsing = 0;
+
+                static float cameraProjection[16];
+
+                static float fov = 27.f;
+                Perspective(fov, io.DisplaySize.x / io.DisplaySize.y, 0.1f, 100.f, cameraProjection);
+
+                static float camYAngle = 165.f / 180.f * 3.14159f;
+                static float camXAngle = 32.f / 180.f * 3.14159f;
+
+                bool viewDirty = false;
+                static bool firstFrame = true;
+
+
+                viewDirty |= ImGui::SliderFloat("Distance", &camDistance, 1.f, 10.f);
+                if (viewDirty || firstFrame)
+                {
+                    float eye[] = { cosf(camYAngle) * cosf(camXAngle) * camDistance, sinf(camXAngle) * camDistance, sinf(camYAngle) * cosf(camXAngle) * camDistance };
+                    float at[] = { 0.f, 0.f, 0.f };
+                    float up[] = { 0.f, 1.f, 0.f };
+                    LookAt(eye, at, up, cameraView);
+                    firstFrame = false;
+                }
+
+                EditTransform(cameraView, cameraProjection, objectMatrix[matId], lastUsing == matId);
+                if (ImGuizmo::IsUsing())
+                {
+                    lastUsing = matId;
+                }
+                // END - 3D DISPLAY
+
                 ImGui::End();
             }
 
@@ -566,4 +724,203 @@ int ExtractUint8FromCanTelegram(uint8_t telegram[], uint8_t sizeOfTelegram, uint
     }
     *output = (val >> spnConfig.bit) & mask;
     return 0;
+}
+
+
+void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bool editTransformDecomposition)
+{
+    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
+    static bool useSnap = false;
+    static float snap[3] = { 1.f, 1.f, 1.f };
+    static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
+    static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
+    static bool boundSizing = false;
+    static bool boundSizingSnap = false;
+
+    if (editTransformDecomposition)
+    {
+        if (ImGui::IsKeyPressed(ImGuiKey_T))
+            mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+        if (ImGui::IsKeyPressed(ImGuiKey_E))
+            mCurrentGizmoOperation = ImGuizmo::ROTATE;
+        if (ImGui::IsKeyPressed(ImGuiKey_R)) // r Key
+            mCurrentGizmoOperation = ImGuizmo::SCALE;
+        if (ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
+            mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+        ImGui::SameLine();
+        if (ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
+            mCurrentGizmoOperation = ImGuizmo::ROTATE;
+        ImGui::SameLine();
+        if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
+            mCurrentGizmoOperation = ImGuizmo::SCALE;
+        if (ImGui::RadioButton("Universal", mCurrentGizmoOperation == ImGuizmo::UNIVERSAL))
+            mCurrentGizmoOperation = ImGuizmo::UNIVERSAL;
+        float matrixTranslation[3], matrixRotation[3], matrixScale[3];
+        //ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
+        //ImGui::InputFloat3("Tr", matrixTranslation);
+        //ImGui::InputFloat3("Rt", matrixRotation);
+        //ImGui::InputFloat3("Sc", matrixScale);
+        //ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
+
+        if (mCurrentGizmoOperation != ImGuizmo::SCALE)
+        {
+            if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
+                mCurrentGizmoMode = ImGuizmo::LOCAL;
+            ImGui::SameLine();
+            if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
+                mCurrentGizmoMode = ImGuizmo::WORLD;
+        }
+        if (ImGui::IsKeyPressed(ImGuiKey_S))
+            useSnap = !useSnap;
+        ImGui::Checkbox("##UseSnap", &useSnap);
+        ImGui::SameLine();
+
+        switch (mCurrentGizmoOperation)
+        {
+        case ImGuizmo::TRANSLATE:
+            ImGui::InputFloat3("Snap", &snap[0]);
+            break;
+        case ImGuizmo::ROTATE:
+            ImGui::InputFloat("Angle Snap", &snap[0]);
+            break;
+        case ImGuizmo::SCALE:
+            ImGui::InputFloat("Scale Snap", &snap[0]);
+            break;
+        }
+        ImGui::Checkbox("Bound Sizing", &boundSizing);
+        if (boundSizing)
+        {
+            ImGui::PushID(3);
+            ImGui::Checkbox("##BoundSizing", &boundSizingSnap);
+            ImGui::SameLine();
+            ImGui::InputFloat3("Snap", boundsSnap);
+            ImGui::PopID();
+        }
+    }
+
+    ImGuiIO& io = ImGui::GetIO();
+    float viewManipulateRight = io.DisplaySize.x;
+    float viewManipulateTop = 0;
+    static ImGuiWindowFlags gizmoWindowFlags = 0;
+    if (useWindow)
+    {
+        ImGui::SetNextWindowSize(ImVec2(800, 400), ImGuiCond_Appearing);
+        ImGui::SetNextWindowPos(ImVec2(400, 20), ImGuiCond_Appearing);
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, (ImVec4)ImColor(0.35f, 0.3f, 0.3f));
+        ImGui::Begin("Gizmo", 0, gizmoWindowFlags);
+        ImGuizmo::SetDrawlist();
+        float windowWidth = (float)ImGui::GetWindowWidth();
+        float windowHeight = (float)ImGui::GetWindowHeight();
+        ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, windowWidth, windowHeight);
+        viewManipulateRight = ImGui::GetWindowPos().x + windowWidth;
+        viewManipulateTop = ImGui::GetWindowPos().y;
+        ImGuiWindow* window = ImGui::GetCurrentWindow();
+        gizmoWindowFlags = ImGui::IsWindowHovered() && ImGui::IsMouseHoveringRect(window->InnerRect.Min, window->InnerRect.Max) ? ImGuiWindowFlags_NoMove : 0;
+    }
+    else
+    {
+        ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+    }
+
+    ImGuizmo::DrawGrid(cameraView, cameraProjection, identityMatrix, 100.f);
+    ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], gizmoCount);
+    ImGuizmo::Manipulate(cameraView, cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode, matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
+
+    ImGuizmo::ViewManipulate(cameraView, camDistance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
+
+    if (useWindow)
+    {
+        ImGui::End();
+        ImGui::PopStyleColor(1);
+    }
+}
+
+
+void Perspective(float fovyInDegrees, float aspectRatio, float znear, float zfar, float* m16)
+{
+    float ymax, xmax;
+    ymax = znear * tanf(fovyInDegrees * 3.141592f / 180.0f);
+    xmax = ymax * aspectRatio;
+    Frustum(-xmax, xmax, -ymax, ymax, znear, zfar, m16);
+}
+
+
+void Frustum(float left, float right, float bottom, float top, float znear, float zfar, float* m16)
+{
+    float temp, temp2, temp3, temp4;
+    temp = 2.0f * znear;
+    temp2 = right - left;
+    temp3 = top - bottom;
+    temp4 = zfar - znear;
+    m16[0] = temp / temp2;
+    m16[1] = 0.0;
+    m16[2] = 0.0;
+    m16[3] = 0.0;
+    m16[4] = 0.0;
+    m16[5] = temp / temp3;
+    m16[6] = 0.0;
+    m16[7] = 0.0;
+    m16[8] = (right + left) / temp2;
+    m16[9] = (top + bottom) / temp3;
+    m16[10] = (-zfar - znear) / temp4;
+    m16[11] = -1.0f;
+    m16[12] = 0.0;
+    m16[13] = 0.0;
+    m16[14] = (-temp * zfar) / temp4;
+    m16[15] = 0.0;
+}
+
+
+void LookAt(const float* eye, const float* at, const float* up, float* m16)
+{
+    float X[3], Y[3], Z[3], tmp[3];
+
+    tmp[0] = eye[0] - at[0];
+    tmp[1] = eye[1] - at[1];
+    tmp[2] = eye[2] - at[2];
+    Normalize(tmp, Z);
+    Normalize(up, Y);
+
+    Cross(Y, Z, tmp);
+    Normalize(tmp, X);
+
+    Cross(Z, X, tmp);
+    Normalize(tmp, Y);
+
+    m16[0] = X[0];
+    m16[1] = Y[0];
+    m16[2] = Z[0];
+    m16[3] = 0.0f;
+    m16[4] = X[1];
+    m16[5] = Y[1];
+    m16[6] = Z[1];
+    m16[7] = 0.0f;
+    m16[8] = X[2];
+    m16[9] = Y[2];
+    m16[10] = Z[2];
+    m16[11] = 0.0f;
+    m16[12] = -Dot(X, eye);
+    m16[13] = -Dot(Y, eye);
+    m16[14] = -Dot(Z, eye);
+    m16[15] = 1.0f;
+}
+
+void Normalize(const float* a, float* r)
+{
+    float il = 1.f / (sqrtf(Dot(a, a)) + FLT_EPSILON);
+    r[0] = a[0] * il;
+    r[1] = a[1] * il;
+    r[2] = a[2] * il;
+}
+
+float Dot(const float* a, const float* b)
+{
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+void Cross(const float* a, const float* b, float* r)
+{
+    r[0] = a[1] * b[2] - a[2] * b[1];
+    r[1] = a[2] * b[0] - a[0] * b[2];
+    r[2] = a[0] * b[1] - a[1] * b[0];
 }
